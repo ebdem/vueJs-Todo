652a7bd8e118ff83c62e744ef52d86cad25e336e
ebdem <ebu.bekir.3333@gmail.com>
2020-12-21 01:20:41 +0300

    first also last commit for todo list and also i used vue jse

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,98 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="container">
+    <div class="row">
+      <div class="col-md-8 offset-md-2 text-center">
+        <h2 class="mt-5">ToDo List | Vue Js</h2>
+        <hr />
+        <div class="row">
+          <div
+            class="col-md-6 offset-md-3 pt-2 pb-2 d-flex flex-row align-items-center justify-content-between"
+          >
+            <input type="text" v-model="todoText" />
+            <button @click="addTodo()" class="btn btn-primary">
+              Ekle
+            </button>
+          </div>
+        </div>
+        <hr />
+        <div class="todo-container">
+          <Todo
+            @deleteTodo="deleteTodo($event)"
+            v-for="todo in todoList"
+            :todo="todo"
+            :key="todo.id"
+          />
+        </div>
+      </div>
+    </div>
   </div>
-  <router-view />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Todo from "@/components/Todo";
+import axios from "axios";
+export default {
+  components: {
+    Todo
+  },
+  data() {
+    return {
+      todoText: "",
+      todoList: []
+    };
+  },
+  methods: {
+    addTodo() {
+      axios
+        .post(
+          "https://vuejs-todo-6802a-default-rtdb.firebaseio.com/todoList.json",
+          {
+            text: this.todoText
+          }
+        )
+        .then(response => {
+          console.log(response);
+          this.todoList.push({
+            id: response.data.name,
+            text: this.todoText
+          });
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    deleteTodo(todoId) {
+      axios
+        .delete(
+          "https://vuejs-todo-6802a-default-rtdb.firebaseio.com/todoList/" +
+            todoId +
+            ".json"
+        )
+        .then(() => {
+          let index = this.todoList.findIndex(i => {
+            return i.id == todoId;
+          });
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+          this.todoList.splice(index, 1);
+        })
+        .catch(e => console.log(e));
+    }
+  },
+  created() {
+    axios
+      .get("https://vuejs-todo-6802a-default-rtdb.firebaseio.com/todoList.json")
+      .then(response => {
+        for (let key in response.data) {
+          //this.todoList.push(response.data[key]);
+          let todo = {
+            text: response.data[key].text,
+            id: key
+          };
+          //console.log(response.data[key]);
+          this.todoList.push(todo);
+        }
+      })
+      .catch(e => console.log(e));
+  }
+};
+</script>
---
+++ src/components/Todo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="row">
+    <div
+      class="col-md-6 offset-md-3 pt-2 pb-2 border d-flex flex-row align-items-center justify-content-between"
+    >
+      <span>{{ todo.text }}</span>
+      <button class="btn btn-danger" @click="$emit('deleteTodo', todo.id)">
+        Sil
+      </button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ["todo"]
+};
+</script>
